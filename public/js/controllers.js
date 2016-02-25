7b1aa39433aa0da8c38d38c4852586c40b58fbdf
MARCO AURELIO SARAIVA JUNIOR <marcoa2009_00@unicoob.local>
2016-02-25 13:51:07 -0300

    init

---
+++ public/js/controllers.js
@@ -0,0 +1,14 @@
+var bookWishlistAppControllers = angular.module('bookWishlistAppControllers', []);
+
+bookWishlistAppControllers.controller('LoginController', ['$scope','$http', function($scope, $http){
+
+}]);
+
+bookWishlistAppControllers.controller('SignupController', ['$scope','$http', function($scope, $http){
+
+}]);
+
+bookWishlistAppControllers.controller('MainController', ['$scope','$http', function($scope, $http){
+
+}]);
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html ng-app="bookWishlistApp">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Books</title>
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<script src="bower_components/angular/angular.min.js"></script>
+    <script src="bower_components/lodash/lodash.js"></script>
+    <script src="bower_components/angular-route/angular-route.min.js"></script>
+    <script src="bower_components/angular-local-storage/dist/angular-local-storage.min.js"></script>
+    <script src="bower_components/restangular/dist/restangular.min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/controllers.js"></script>
+    <style>        
+        li { padding-bottom: 8px; }
+    </style>
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Book List</h1>
+			</div>
+		</div>
+
+		<div ng-view></div>
+
+	</div>
+
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>	
+</body>
+</html>
